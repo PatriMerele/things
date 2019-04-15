d853be56bd9ad4faa91c2f2823018893f66f52f9
PC04 <patriciomerele@gmail.com>
2019-04-15 15:57:40 -0300

    15-4

---
+++ patrimerele 6to 7ma/Bolillero/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bolillero;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Bolilleroo bolillero = new Bolilleroo();
+            Simulacion simulacion = new Simulacion();
+        }
+    }
+}
