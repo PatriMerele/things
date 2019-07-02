2861686c2f98716f28038d150aa92bbc504fc936
PC04 <patriciomerele@gmail.com>
2019-07-02 15:44:12 -0300

    asdf
    asdf

---
+++ patrimerele 6to 7ma/dados/dados/Dado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dados
+{
+    public class Dado
+    {
+        public byte cantidadDeCaras { get; set; }
+        public byte caraSuperior { get; private set; }
+
+        public Dado()
+        {
+
+        }
+        public void tirar()
+        {
+
+        }
+        public bool esCaraSuperior(byte unaCaraSuperior)
+        {
+
+        }
+    }
+}
---
+++ patrimerele 6to 7ma/dados/dados/Vaso.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dados
+{
+    public class Vaso
+    {
+        public List<Dado> dados { get; set; }
+
+        public Vaso()
+        {
+
+        }
+
+        public void tirar()
+        {
+
+        }
+        public bool esCaraSuperior(byte unaCaraSuperior)
+        {
+
+        }
+        public void agregarDado(Dado dado)
+        {
+
+        }
+    }
+}
