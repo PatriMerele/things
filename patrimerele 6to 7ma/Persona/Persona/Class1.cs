6fa96cae33b770462cb4007d96eb3c60d9ac85a4
L21-PC03 <patriciomerele@gmail.com>
2019-08-08 15:49:33 -0300

    08-08

---
+++ patrimerele 6to 7ma/Persona/Persona/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Persona
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ patrimerele 6to 7ma/Persona/Persona/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Persona
+{
+    [Table ("Persona")]
+    public class Persona
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("dni")]
+        [Key]
+        public int Dni { get; set; }
+
+
+    }
+}
